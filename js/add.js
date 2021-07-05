c9599d92ca124384915dad4c6a52f1d7111d2a05
Aleksey Kononov <alekseykon1993@gmail.com>
2021-07-05 15:07:41 +0700

    Resume 1.3

--- js/add.js
+++ js/add.js
@@ -2,46 +2,6 @@
 
 window.addEventListener("DOMContentLoaded", function() {
 
-    // Tabs---------------------------------------------------------------------------
-
-    const tab = document.querySelectorAll(".nav__item"),
-          tabContent = document.querySelectorAll(".main__tabs"),
-          tabParent = document.querySelector(".nav");
-
-    function hideTabContent() {
-        tabContent.forEach(function(item) {
-            item.classList.add("hide");
-            item.classList.remove("show");
-        });
-        tab.forEach(function(item) {
-            item.classList.remove("nav__item_active");
-        });
-    }
-
-    function showTabContent(item = 0) {
-        tabContent[item].classList.add("show");
-        tabContent[item].classList.remove("hide");
-        tab[item].classList.add("nav__item_active");
-    }
-
-    hideTabContent();
-    showTabContent();
-
-    tabParent.addEventListener("click", function(event) {
-        if (event.target && !event.target.classList.contains("nav__item_active")) {
-            if (event.target && event.target.classList.contains("nav__item")) {
-                tab.forEach(function(item, index) {
-                    if (event.target == item) {
-                        hideTabContent();
-                        showTabContent(index);
-                    } 
-                });
-            }
-        } else {
-            return;
-        }
-    });
-
 // ModalWindow-------------------------------------------------------------------
 
     const   modalWindow = document.querySelector(".modal"),
@@ -90,18 +50,18 @@ window.addEventListener("DOMContentLoaded", function() {
 
 //ImageWindow-----------------------------------------------------------------------------
 
-    const avatarSection = document.querySelector(".header__avatar"),
-          modalWindowImage = document.querySelector(".modal__image"),
-          sectionImg = document.querySelector(".modal__image-img");
+//     const avatarSection = document.querySelector(".header__avatar"),
+//           modalWindowImage = document.querySelector(".modal__image"),
+//           sectionImg = document.querySelector(".modal__image-img");
 
-    avatarSection.addEventListener("click", function(event) {
-        if (event.target && event.target.classList.contains("header__avatar-img")) {
+//     avatarSection.addEventListener("click", function(event) {
+//         if (event.target && event.target.classList.contains("header__avatar-img")) {
             
-            sectionImg.innerHTML = `<img src="${event.target.currentSrc}" alt="">`;
-            showModalWindow(modalWindowImage);
-        }
-    });
+//             sectionImg.innerHTML = `<img src="${event.target.currentSrc}" alt="">`;
+//             showModalWindow(modalWindowImage);
+//         }
+//     });
 
-   Close(modalWindowImage);
+//    Close(modalWindowImage);
 
 });
